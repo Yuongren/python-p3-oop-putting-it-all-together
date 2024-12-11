678d66c47633a9495b6f190795aec2e0dc6ff6ae
gitYoungren <youngrengitonga@gmail.com>
2024-12-11 16:38:13 +0300

    completed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,30 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title  # Calls the setter for title
+        self.page_count = page_count  # Calls the setter for page_count
+
+    @property
+    def title(self):
+        """The title property"""
+        return self._title  # Return the stored title value
+
+    @title.setter
+    def title(self, value):
+        """Setter for the title"""
+        self._title = value  # Set the title
+
+    @property
+    def page_count(self):
+        """The page_count property"""
+        return self._page_count  # Return the stored page count
+
+    @page_count.setter
+    def page_count(self, value):
+        """Setter for the page_count. Ensures the page_count is an integer."""
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value  # Set the page count only if it's an integer
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand  
+        self.size = size  
+        self.condition = "New"  
+
+    @property
+    def brand(self):
+        """The brand property."""
+        return self._brand 
+
+    @brand.setter
+    def brand(self, value):
+        """Setter for the brand."""
+        self._brand = value
+
+    @property
+    def size(self):
+        """The size property."""
+        return self._size  
+    
+    @size.setter
+    def size(self, value):
+        """Setter for the size. Ensures size is an integer."""
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        """Repairs the shoe and sets its condition to 'New'."""
+        self.condition = "New"
+        print("Your shoe is as good as new!")
